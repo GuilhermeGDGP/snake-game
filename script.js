3b5b0cc1c7d5394ecb531def44f03b7f3645789c
GuilhermeGDGP <guilhermedgoncalvez@gmail.com>
2020-11-25 10:00:16 -0300

    Definindo os movimentos da cobra

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = 'right';
 
 function createBG() {
     context.fillStyle = 'lightgreen';
@@ -19,5 +20,43 @@ function createSnake() {
     }
 }
 
-createBG();
-createSnake();
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if (event.keyCode == 37 && direction != 'right') direction = 'left';
+    if (event.keyCode == 38 && direction != 'up') direction = 'down';
+    if (event.keyCode == 39 && direction != 'left') direction = 'right';
+    if (event.keyCode == 40 && direction != 'down') direction = 'up';
+}
+
+function checkBoardCollide() {
+    if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
+    if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == 'up') snake[0].y = 0;
+    if (snake[0].y < 0 && direction == 'down') snake[0].y = 16 * box;
+}
+
+function startGame() {
+    checkBoardCollide();
+    createBG();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == 'right') snakeX += box;
+    if (direction == 'left') snakeX -= box;
+    if (direction == 'up') snakeY += box;
+    if (direction == 'down') snakeY -= box;
+
+    snake.pop();
+
+    let newHaed = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHaed);
+}
+
+let game = setInterval(startGame, 100);
